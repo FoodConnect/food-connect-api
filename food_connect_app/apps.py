98ea18de8453a06530d8be81335493ca2a7ff463
Leah Perri <perrileah2023@gmail.com>
2024-03-09 16:15:10 -0600

    creates a signal for model creation of donor or charity based on user role

--- food_connect_app/apps.py
+++ food_connect_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class FoodConnectAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'food_connect_app'
+
+    def ready(self):
+        import food_connect_app.signals
