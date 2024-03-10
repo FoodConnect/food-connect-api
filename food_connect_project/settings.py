2d897d338cec30279969b34c673d4bcdc311dcb3
Leah Perri <perrileah2023@gmail.com>
2024-03-09 19:17:38 -0600

    settings adjustment

--- food_connect_project/settings.py
+++ food_connect_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#6*nx892*qk1z2mg$q6@r6d_2ehp6r!l6z&!9qhi)7*-os@()%'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
