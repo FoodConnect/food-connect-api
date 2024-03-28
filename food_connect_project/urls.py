a746dee29ae8018f3fb2e63a3e65a5e60d7b9e82
Leah Perri <perrileah2023@gmail.com>
2024-03-27 21:26:57 -0500

    small edits on urls and http requests

--- food_connect_project/urls.py
+++ food_connect_project/urls.py
@@ -40,7 +40,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="rest_login"),
     path("logout/", LogoutView.as_view(), name="rest_logout"),
     path("user/", UserDetailsView.as_view(), name="rest_user_details"),
-    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-    path("token/refresh/", get_refresh_view().as_view(), name="token_refresh"),
 ]
 
