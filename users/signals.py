d12dd61978f87a7037dad01bd1788ba6069f6747
Leah Perri <perrileah2023@gmail.com>
2024-03-28 20:38:30 -0500

    cleans up comments

--- users/signals.py
+++ users/signals.py
@@ -11,15 +11,3 @@ def create_profile(sender, instance, created, **kwargs):
         elif instance.role == UserRole.CHARITY.value:
             Charity.objects.create(user=instance)
 
-
-# @receiver(pre_save, sender=User)
-# def create_profile(sender, instance, **kwargs):
-#     # Check if the user is being created (new user) or updated (existing user)
-#     if instance.pk is None:  # If primary key is None, it's a new user being created
-#         if instance.role == UserRole.DONOR.value:
-#             Donor.objects.create(user=instance)
-#         elif instance.role == UserRole.CHARITY.value:
-#             Charity.objects.create(user=instance)
-#     else:  # Existing user being updated
-#         # Add logic to handle updates if needed
-#         pass
